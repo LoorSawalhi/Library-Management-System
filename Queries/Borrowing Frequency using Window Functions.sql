1a1fd008440927793f217c00b64ce53f2a0096f9
Loor Sawalhi <91731396+LoorSawalhi@users.noreply.github.com>
2024-05-02 12:21:34 +0300

    Retrieve borrowing frequency using Window Functions

---
+++ Queries/Borrowing Frequency using Window Functions.sql
@@ -0,0 +1,13 @@
+with borrowing_Frequency AS(
+    SELECT  L.borrower_id,
+             COUNT(*) OVER(PARTITION BY L.borrower_id) AS borrowing_frequency
+    FROM Borrower b
+    JOIN loan L ON b.id = L.borrower_id
+    )
+
+SELECT L.borrower_id,
+       BF.borrowing_frequency ,
+       RANK() OVER(ORDER BY BF.borrowing_frequency ) AS rank
+FROM Borrower b
+JOIN loan L ON b.id = L.borrower_id
+JOIN borrowing_Frequency BF ON BF.borrower_id = L.borrower_id;
