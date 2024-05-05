7f6a1cd89cb58e431f22a4a7888ed30aa05beed9
Loor Sawalhi <91731396+LoorSawalhi@users.noreply.github.com>
2024-05-05 11:22:24 +0300

    Update list of borrowed books query

--- Queries/List of Borrowed Books.sql
+++ Queries/List of Borrowed Books.sql
@@ -1,5 +1,7 @@
 -- List of Borrowed Books: Retrieve all books borrowed by a specific borrower, including those currently unreturned.
 
 select book.id, book.title, book.author, book.state
-from Borrower brw, Books book, loan l
+from Borrower brw
+join loan l on brw.id = l.borrower_id
+join Books book on l.book_id = book.id
 where brw.id = l.borrower_id and book.id = l.book_id and brw.id = 200;
