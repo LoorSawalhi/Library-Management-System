7b52f542b2643c521491e245200da63a7e3daf1e
Loor Sawalhi <91731396+LoorSawalhi@users.noreply.github.com>
2024-05-06 09:33:54 +0300

    Edit borrowing_frequency CTE

--- Queries/Genre Preference by Age using Group By and Having.sql
+++ Queries/Genre Preference by Age using Group By and Having.sql
@@ -9,39 +9,19 @@ WITH AgeGroups AS (
             WHEN AgeYears BETWEEN 26 AND 40 THEN '26-40'
             ELSE 'Over 40'
         END AS AgeRange,
-        AgeYears,
         id
     FROM
-        (SELECT
-            DATEDIFF(year, b.date_of_birth, GETDATE()) AS AgeYears,
-            b.id
-         FROM
-            Borrower b
-        ) AS BorrowerAge
+        (SELECT DATEDIFF(year, b.date_of_birth, GETDATE()) AS AgeYears, b.id
+         FROM Borrower b  ) AS BorrowerAge
 ), GenreGroups AS (
-    SELECT AgeRange, book.genre, COUNT(*) AS genreCount
-    FROM
-        Borrower brw
-    JOIN
-        AgeGroups ON brw.id = AgeGroups.id
-    JOIN
-        Loan l ON l.borrower_id = brw.id
-    JOIN
-        Books book ON book.id = l.book_id
-    GROUP BY
-        AgeRange,
-        book.genre)
+    SELECT ag.AgeRange, book.genre, COUNT(*) AS genreCount
+    FROM AgeGroups ag
+    JOIN Loan l ON l.borrower_id = ag.id
+    JOIN Books book ON book.id = l.book_id
+    GROUP BY AgeRange, book.genre)
 
 SELECT AgeRange, genreCount,genre
-FROM GenreGroups
+FROM GenreGroups g
 GROUP BY AgeRange, genre, genreCount
-HAVING genreCount = (SELECT  MAX(genreCount)
-                      FROM GenreGroups gg WHERE gg.AgeRange = GenreGroups.AgeRange);
-
-
-
-select brw.id, brw.date_of_birth, book.genre
-from Books book
-join loan l on book.id = l.book_id
-join Borrower brw on l.borrower_id = brw.id
-where book.id = 354 or book.ID = 451 or book.id = 414;
+HAVING genreCount = (SELECT MAX(genreCount)
+                      FROM GenreGroups gg WHERE gg.AgeRange = g.AgeRange);
